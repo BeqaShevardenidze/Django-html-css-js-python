c4a8bfed67dc05cc3da84a70481adca61fd6c486
unknown <BeqaShevardenidze>
2024-10-29 10:54:25 +0400

    7

---
+++ 7 over lecture 1 html/html_app/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.html_home, name='html_home'),
+    path('/html_home_video_1', views.html_home_video_1, name='html_home_video_1'),
+]
