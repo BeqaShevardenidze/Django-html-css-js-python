bfebb08053e6d8217cf9425c68a8eaa6a01e446a
unknown <BeqaShevardenidze>
2024-11-01 01:23:11 +0400

    added button img block none

--- main/html_app/static/html_app/js/html_home_video_2.js
+++ main/html_app/static/html_app/js/html_home_video_2.js
@@ -1,1 +1,50 @@
-alert('test')
+// const img_visible_1 = document.querySelector('.img_visible_1')
+
+// const btn_visible_1 = document.querySelector('.btn_visible_1')
+
+// function f_img_visible_1(){
+//     if (img_visible_1.style.display === 'block'){
+//         img_visible_1.style.display = 'none'
+//     } else{
+//         img_visible_1.style.display = 'block'
+//     }
+// }
+
+// btn_visible_1.addEventListener('click', f_img_visible_1)
+
+
+
+const btn_visible_1 = document.querySelector('.btn_visible_1')
+
+const btn_visible_2 = document.querySelector('.btn_visible_2')
+
+const btn_visible_3 = document.querySelector('.btn_visible_3')
+
+const btn_visible_4 = document.querySelector('.btn_visible_4')
+
+const btn_visible_5 = document.querySelector('.btn_visible_5')
+
+function f_test(html_class){
+    const img_visible_1 = document.querySelector(html_class)
+    
+    function f_img_visible_1(){
+        if (img_visible_1.style.display === 'block'){
+            img_visible_1.style.display = 'none'
+        } else{
+            img_visible_1.style.display = 'block'
+        }
+    }
+    f_img_visible_1()
+}
+
+
+btn_visible_1.addEventListener('click', () => f_test('.img_visible_1'));
+
+btn_visible_2.addEventListener('click', () => f_test('.img_visible_2'));
+
+btn_visible_3.addEventListener('click', () => f_test('.img_visible_3'));
+
+btn_visible_4.addEventListener('click', () => f_test('.img_visible_4'));
+
+btn_visible_5.addEventListener('click', () => f_test('.img_visible_5'));
+btn_visible_5.addEventListener('click', () => f_test('.img_visible_6'));
--- main/html_app/static/html_app/js/js.js
+++ main/html_app/static/html_app/js/js.js
@@ -1,17 +1,16 @@
 document.getElementById('runCode').addEventListener('click', function() {
-    // Получаем HTML код
     const htmlContent = document.getElementById('htmlCode').value;
     
-    // Обновляем содержимое блока #livePreview с новым HTML
+    
     const previewFrame = document.getElementById('livePreview');
     previewFrame.innerHTML = htmlContent;
     
-    // Создаем <style> элемент для добавления CSS
+
     const styleTag = document.createElement('style');
     styleTag.innerHTML = cssContent;
     document.head.appendChild(styleTag);
     
-    // Выполняем JavaScript код с использованием функции eval
+
     try {
         eval(jsContent);
     } catch (error) {
