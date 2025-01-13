8fd3b4b8ae57659c864ca4f9996a0674750a4b0c
unknown <BeqaShevardenidze>
2025-01-13 11:41:39 +0400

    buttons

--- main/html_app/static/html_app/js/html_home_video_2.js
+++ main/html_app/static/html_app/js/html_home_video_2.js
@@ -55,21 +55,5 @@ btn_visible_7.addEventListener('click', () => f_test('.img_visible_7'));
 
 
 
-// ------------for image zoom----------------
-function f_zoomed(html_class){
-    document.getElementById(html_class).addEventListener('click',
-        function(){
-            this.classList.toggle('zoomed');
-        }
-    )
-}
-f_zoomed('image')
-f_zoomed('image2')
-f_zoomed('image3')
-f_zoomed('image4')
-f_zoomed('image5')
-f_zoomed('image6')
-f_zoomed('image7')
-
 
 
--- main/html_app/static/html_app/js/html_home_video_2_file_2.js
+++ main/html_app/static/html_app/js/html_home_video_2_file_2.js
@@ -1,3 +1,16 @@
+// ------------Start New Project----------------
+const start_new_project_btn = document.querySelector('.start_new_project_btn')
+function f_start_new_project_btn(){
+    const start_new_project_ul = document.querySelector('.start_new_project_ul')
+    if (start_new_project_ul.style.display === 'block'){
+        start_new_project_ul.style.display = 'none'
+    } else {
+        start_new_project_ul.style.display = 'block'
+    }
+}
+start_new_project_btn.addEventListener('click', () => f_start_new_project_btn());
+
+
 // ------------HTML Structure UnHide----------------
 const html_doc_struct_btn = document.querySelector('.html_doc_struct_btn')
 function f_html_doc_struct_btn(){
@@ -9,3 +22,16 @@ function f_html_doc_struct_btn(){
     }
 }
 html_doc_struct_btn.addEventListener('click', () => f_html_doc_struct_btn());
+
+
+// ------------HTML Tags UnHide----------------
+const html_doc_tags_btn = document.querySelector('.html_doc_tags')
+function f_html_doc_tags_btn(){
+    const html_doc_tags_ul = document.querySelector('.html_doc_tags_ul')
+    if (html_doc_tags_ul.style.display === 'block'){
+        html_doc_tags_ul.style.display = 'none'
+    } else {
+        html_doc_tags_ul.style.display = 'block'
+    }
+}
+html_doc_tags_btn.addEventListener('click', () => f_html_doc_tags_btn());
