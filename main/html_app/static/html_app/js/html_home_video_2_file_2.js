51c939121504381bf71d2dbf7d84feff2abc8586
unknown <BeqaShevardenidze>
2025-01-30 17:02:41 +0400

    'img_tag_1'

--- main/html_app/static/html_app/js/html_home_video_2_file_2.js
+++ main/html_app/static/html_app/js/html_home_video_2_file_2.js
@@ -35,3 +35,19 @@ function f_html_doc_tags_btn(){
     }
 }
 html_doc_tags_btn.addEventListener('click', () => f_html_doc_tags_btn());
+
+
+// ------------HTML Tags UnHide----------------
+// html_doc_img
+const html_doc_img_btn = document.querySelector('.html_doc_img')
+function f_html_doc_img_btn(){
+    const html_doc_img_ul = document.querySelector('.html_doc_img_ul')
+    if (html_doc_img_ul.style.display === 'block'){
+        html_doc_img_ul.style.display = 'none'
+    } else {
+        html_doc_img_ul.style.display = 'block'
+    }
+}
+html_doc_img_btn.addEventListener('click', () => f_html_doc_img_btn());
+
+
